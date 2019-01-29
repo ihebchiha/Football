d8df81c760275ea431f7706e7cb6252f53d34d81
Iheb Chiha <iheb.chiha@proxym-it.com>
2019-01-29 16:34:30 +0100

    update League.java

--- src/com/company/League.java
+++ src/com/company/League.java
@@ -8,4 +8,12 @@ public class League {
     ArrayList<Team> teams;
     HashMap<Team,Team> fixtures;
 
+
+    public void initializeTeams() {
+        // We are going to use files to initialize teams
+    }
+
+    public void setFixtures(HashMap<Team, Team> fixtures) {
+        this.fixtures = fixtures;
+    }
 }
