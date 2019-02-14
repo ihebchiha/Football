6b6df2046ddd959c74e7d1471eb6d677b83d403d
Iheb Chiha <iheb.chiha@proxym-it.com>
2019-02-14 16:10:53 +0100

    update project

--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -4,6 +4,7 @@ import com.company.exceptions.PlayerNotFound;
 import com.company.util.OpsImpl;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Team {
 
@@ -16,10 +17,29 @@ public class Team {
     public Team() {
     }
 
+    public Team(int idTeam, String nameTeam, ArrayList<Player> players, Coach coach, String formation) {
+        this.idTeam = idTeam;
+        this.nameTeam = nameTeam;
+        this.players = players;
+        this.coach = coach;
+        this.formation = formation;
+    }
+
     public Team(int idTeam, String nameTeam, ArrayList<Player> players, String file) {
         this.idTeam = idTeam;
         this.nameTeam = nameTeam;
         this.players = (ArrayList<Player>) initializePlayers(file, players);
+    }
+
+    public Team createTeam(ArrayList<Player> playerz) {
+        System.out.println("Create your team:");
+        Scanner sc = new Scanner(System.in);
+        int id = sc.nextInt();
+        String teamName = sc.nextLine();
+        String formation = sc.nextLine();
+        Coach coach = new Coach("coach", "", "");
+
+        return new Team(id, teamName, playerz, coach, formation);
     }
 
     public ArrayList<Player> initializePlayers(String file, ArrayList<Player> players) {
@@ -49,6 +69,22 @@ public class Team {
         this.players = players;
     }
 
+    public Coach getCoach() {
+        return coach;
+    }
+
+    public void setCoach(Coach coach) {
+        this.coach = coach;
+    }
+
+    public String getFormation() {
+        return formation;
+    }
+
+    public void setFormation(String formation) {
+        this.formation = formation;
+    }
+
     public void searchPlayerByName(String name) throws PlayerNotFound {
         for (Player player : players) {
             if (player.getFullname().toLowerCase() == name.toLowerCase()) {
@@ -56,7 +92,7 @@ public class Team {
                 player.showPlayerInfos();
                 break;
             } else {
-                throw new PlayerNotFound("Player doesn't play for this Team");
+                System.out.println(new PlayerNotFound("Player doesn't play for this Team").getMessage());
             }
         }
     }
@@ -67,6 +103,8 @@ public class Team {
                 System.out.println("Player Found");
                 player.showPlayerInfos();
                 break;
+            } else {
+                System.out.println(new PlayerNotFound("There is no player with this number in the squad").getMessage());
             }
         }
     }
